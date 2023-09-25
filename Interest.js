073f4d08af254ad4cfd0531f215c52e803e2c69e
MakyJame <ngoctai1995@gmail.com>
2023-09-25 10:58:06 +0700

    click here

--- Interest.js
+++ Interest.js
@@ -2,33 +2,42 @@ import React, { useState } from 'react';
 
 function Interest() {
     const [principal, setPrincipal] = useState('');
-    const [daysDiff, setDaysDiff] = useState('');
+    const [startDate, setStartDate] = useState('');
     const [result, setResult] = useState('');
 
     const calculateInterest = () => {
-        const p = parseFloat(principal);
-        const diff = parseInt(daysDiff);
-        let interest = 0;
-
-        if (!isNaN(p) && !isNaN(diff)) {
-            if (diff <= 30) {
-                interest = (p * 0.06 * 2) + (p * 0.01);
-            } else if (diff <= 33) {
-                interest = (p * 0.06 * 2) + (p * 0.015);
-            } else if (diff <= 37) {
-                interest = (p * 0.06 * 2) + (p * 0.02);
-            } else if (diff <= 44) {
-                interest = (p * 0.06 * 2) + (p * 0.03);
-            } else if (diff <= 48) {
-                interest = (p * 0.06 * 2) + (p * 0.04);
-            } else if (diff <= 53) {
-                interest = (p * 0.06 * 2) + (p * 0.05);
-            } else {
-                const extraMonths = Math.floor((diff - 30) / 30);
-                interest = (p * 0.06 * 2) + (p * 0.06 * extraMonths);
+        const p = parseInt(principal);
+        const startDateObj = new Date(startDate);
+        startDateObj.setDate(startDateObj.getDate() - 1); // Thêm 1 ngày vào ngày bắt đầu
+        const currentDate = new Date();
+        const daysDiff = Math.floor((currentDate - startDateObj) / (1000 * 60 * 60 * 24));
+        let interestRate = 0;
+        const month = parseInt(daysDiff / 30)
+        const day = parseInt(daysDiff % 30)
+        const oneM = p * 0.06
+        if (!isNaN(p) && !isNaN(daysDiff)) {
+            if (day <= 3) {
+                interestRate = 0.01;
+            } else if (day <= 7) {
+                interestRate = 0.015;
+            } else if (day <= 9) {
+                interestRate = 0.02;
+            } else if (day <= 15) {
+                interestRate = 0.03;
+            } else if (day <= 19) {
+                interestRate = 0.04;
+            } else if (day <= 24) {
+                interestRate = 0.05;
+            } else if (day <= 30) {
+                interestRate = 0.06;
             }
+            // else {
+            //   const extraDays = daysDiff - 30;
+            //   interestRate = 0.06 + (extraDays * 0.01);
+            // }
 
-            setResult(`Số tiền lãi cần đóng: ${interest.toFixed(2)} đồng`);
+            const interest = month * oneM + interestRate * p
+            setResult(`Tổng số ngày: ${daysDiff}. Số tiền lãi cần đóng: ${interest.toFixed(0)} đồng`);
         } else {
             setResult('Vui lòng nhập thông tin hợp lệ.');
         }
@@ -46,13 +55,12 @@ function Interest() {
                 onChange={(e) => setPrincipal(e.target.value)}
             />
             <br />
-            <label htmlFor="daysDiff">Số ngày cầm cố:</label>
+            <label htmlFor="startDate">Ngày bắt đầu cầm:</label>
             <input
-                type="number"
-                id="daysDiff"
-                placeholder="Số ngày"
-                value={daysDiff}
-                onChange={(e) => setDaysDiff(e.target.value)}
+                type="date"
+                id="startDate"
+                value={startDate}
+                onChange={(e) => setStartDate(e.target.value)}
             />
             <br />
             <button onClick={calculateInterest}>Tính lãi</button>
